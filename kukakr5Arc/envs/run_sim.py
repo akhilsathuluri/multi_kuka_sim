9930fd6fc8c9d9c57367b39f6a505d4723673543
akhilsathuluri <akhilsathuluri@gmail.com>
2020-03-28 14:01:46 +0530

    reacher_saved_policy

--- kukakr5Arc/envs/run_sim.py
+++ kukakr5Arc/envs/run_sim.py
@@ -5,6 +5,7 @@ from stable_baselines.ddpg.policies import MlpPolicy
 from stable_baselines.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise, AdaptiveParamNoiseSpec
 from stable_baselines import DDPG
 
+import os
 import pybullet_envs
 import kukakr5Arc
 
@@ -17,17 +18,28 @@ action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=floa
 
 model = DDPG(MlpPolicy, env, verbose=1, param_noise=param_noise, action_noise=action_noise)
 model.learn(total_timesteps=400000)
-model.save("kukakr5Arc_reacher")
+model.save("/home/nightmareforev/git/multi_kuka_sim/kukakr5Arc/envs/saved_policies/kukakr5Arc_reacher")
+print('Saving model.... Model saved')
 
 del model # remove to demonstrate saving and loading
 
-model = DDPG.load("kukakr5Arc_reacher", env = env)
+model = DDPG.load("/home/nightmareforev/git/multi_kuka_sim/kukakr5Arc/envs/saved_policies/kukakr5Arc_reacher", env = env)
+print('Loading model.....Model loaded')
 
 #env.render() goes before env.reset() for the render to work
 #env.render()
+
 obs = env.reset()
 while True:
     action, _states = model.predict(obs)
     obs, rewards, dones, info = env.step(action)
 #    env.render()
+
+while True:
+	obs = env.reset()
+	for moves in range(80):
+		action, _states = model.predict(obs)
+		obs, rewards, dones, info = env.step(action)
+		moves += 1
+
 
